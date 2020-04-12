0e29e4d921790fa39314339347e6580b36f547b1
Woo-sang yun <wsy04018@naver.com>
2020-04-12 22:51:35 +0900

    Updates

---
+++ precache-manifest.51954986536c826ff838beca17c3547a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0614f70d2164323dcfed9d8ecc38f0f0",
+    "url": "/morpheus/index.html"
+  },
+  {
+    "revision": "21a96a83e79a5a59a20d",
+    "url": "/morpheus/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "221b396a5dbd51ee7606",
+    "url": "/morpheus/static/js/2.4814832d.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/morpheus/static/js/2.4814832d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a96a83e79a5a59a20d",
+    "url": "/morpheus/static/js/main.ec8829c4.chunk.js"
+  },
+  {
+    "revision": "df96b4b3e0f458b16591",
+    "url": "/morpheus/static/js/runtime-main.7831b759.js"
+  },
+  {
+    "revision": "2c238dd156d7cc5a348eb08da44a9113",
+    "url": "/morpheus/static/media/logo.2c238dd1.png"
+  },
+  {
+    "revision": "e57613554d6f862ce02f878369f044a8",
+    "url": "/morpheus/static/media/top.e5761355.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0bd749a396cabaef2921892c64b4111.js"
+  "/morpheus/precache-manifest.51954986536c826ff838beca17c3547a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/morpheus/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
